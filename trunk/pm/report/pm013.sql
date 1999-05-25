b0eec7eb89998b97c4a65aac69bf6296fcc27b1c
gpaulissen <gpaulissen@c1c61559-3a1e-0410-b1f8-a6e2dc18b5fa>
1999-05-25 14:45:04 +0000

    Released

---
+++ trunk/pm/report/pm013.sql
@@ -0,0 +1,58 @@
+PROMPT
+PROMPT	$RCSfile$
+REMARK 
+REMARK	$Date$
+REMARK
+REMARK	$Author$
+REMARK
+REMARK  $Revision$
+REMARK
+PROMPT	Description:	This scripts list the modules, actions and usernames of a SQL text.
+REMARK
+PROMPT	Parameters:	
+PROMPT 			1 - Database
+PROMPT			&&1
+PROMPT			2 - Sql id as stored in pm_sql
+PROMPT			&&2
+PROMPT
+REMARK  $Log$
+REMARK  Revision 1.2  1999/05/25 14:45:02  gpaulissen
+REMARK  Released
+REMARK
+REMARK  Revision 1.1  1999/04/29 10:08:16  gpaulissen
+REMARK  Initial revision
+REMARK
+REMARK
+REMARK
+
+define db = &&1
+define sql_id = &&2
+
+set linesize 132
+set trimspool on
+set pagesize 66
+
+VAR	db varchar2(20)
+VAR	sql_id number
+
+begin
+	:db := UPPER('&&db');
+	:sql_id := to_number('&&sql_id');
+end;
+/
+
+column	module			heading "Module"	format a30
+column	action			heading	"Action"	format a30
+column	parsing_user_name	heading "Executed by"	format a30
+column	parsing_schema_name	heading "From schema"	format a30
+
+@@ pm013q1.sql
+
+/
+
+undefine db
+undefine sql_id
+
+undefine 1
+undefine 2
+
