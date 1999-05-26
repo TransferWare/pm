5b3d20222ffd8ee7226c68a92e4bc0875c49ea50
Gert-Jan Paulissen <gert.jan.paulissen@gmail.com>
1999-05-26 15:48:54 +0000

    Initial revision

---
+++ report/pmerpt.sql
@@ -0,0 +1,16 @@
+REMARK
+REMARK	$RCSfile$
+REMARK 
+REMARK	$Date$
+REMARK
+REMARK	$Author$
+REMARK
+REMARK  $Revision$
+REMARK
+REMARK	Description:	end of report
+REMARK
+
+undefine date_format
+
+clear breaks 
+clear columns
