bf68ab68ea8ce9b37155293c4a55172a035dfc6b
Gert-Jan Paulissen <gert.jan.paulissen@gmail.com>
1999-10-07 10:01:00 +0000

    Removed obsolete table

--- report/pm007q1.sql
+++ report/pm007q1.sql
@@ -1,16 +1,12 @@
 select	/* $Header$ */
-        pm_session.username
+        pm_sess_io.username
 ,	sum(pm_sess_io.block_gets)	block_gets
 ,	sum(pm_sess_io.block_changes)	block_changes
 from	pm_sess_io
-,	pm_session
 where	pm_sess_io.db = :db
 and	pm_sess_io.run_id between :lwb_run_id and :upb_run_id
-and	pm_session.db = pm_sess_io.db
-and	pm_session.run_id = pm_sess_io.run_id
-and	pm_session.sid = pm_sess_io.sid
 group by
-	pm_session.username
+	pm_sess_io.username
 having 	sum(pm_sess_io.block_gets) + sum(pm_sess_io.block_changes) >= :minimum
 order by 
 	sum(pm_sess_io.block_gets) desc
