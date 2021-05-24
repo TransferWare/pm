286802b8bdf9c7e7607278ddf70006be4199c28f
gpaulissen <gpaulissen@c1c61559-3a1e-0410-b1f8-a6e2dc18b5fa>
2021-05-24 14:39:55 +0000

    Regenerate using oracle-tools.

--- trunk/pm/sql/src/incr/V20170512160842.0056__DDL_XXYSS_ADMIN.sql
+++
@@ -1,6 +0,0 @@
-/* SQL statement 56 (ALTER;XXYSS_ADMIN;CONSTRAINT;PM_SES_CK2;XXYSS_ADMIN;TABLE;PM_SESSION;;;;;2) */
-ALTER TABLE "XXYSS_ADMIN"."PM_SESSION" ADD CONSTRAINT "PM_SES_CK2" CHECK (
-      ( sql_hash_value is null and sql_address is null ) or
-      ( sql_hash_value is not null and sql_address is not null )
-      ) ENABLE;
-
