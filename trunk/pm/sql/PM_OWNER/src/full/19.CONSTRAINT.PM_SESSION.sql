32de92061ac4951099df44275b4f73b20184ebd4
gpaulissen <gpaulissen@c1c61559-3a1e-0410-b1f8-a6e2dc18b5fa>
2021-06-02 14:21:39 +0000

    Using oracle tools 1.1.0

---
+++ trunk/pm/sql/PM_OWNER/src/full/19.CONSTRAINT.PM_SESSION.sql
@@ -0,0 +1,12 @@
+ALTER TABLE "PM_SESSION" ADD CONSTRAINT "PM_SES_CK1" CHECK (
+      db = upper(db) and
+      run_id >= 1
+      ) ENABLE;
+
+ALTER TABLE "PM_SESSION" ADD CONSTRAINT "PM_SES_CK2" CHECK (
+      ( sql_hash_value is null and sql_address is null ) or
+      ( sql_hash_value is not null and sql_address is not null )
+      ) ENABLE;
+
+ALTER TABLE "PM_SESSION" ADD CONSTRAINT "PM_SES_PK" PRIMARY KEY ("DB", "RUN_ID", "SID") ENABLE;
+
