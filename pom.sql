3feb69a45bcc4d39f76bf1f380fa29e1dd7f9801
paulissoft <paulissoft@gmail.com>
2026-09-23 18:10:02 +0200

    save point

--- pom.sql
+++ pom.sql
@@ -1,11 +1,18 @@
 begin
-  pato_install_pkg.process_project
-  ( p_path => 'admin'
+  admin.pato_cloud_repo.install_file
+  ( p_file_path => 'admin/creusr-no-sqlplus.sql'
+  , p_stop_on_error => true
+  , p_schema => 'ADMIN'
   );
-  pato_install_pkg.process_project
+  admin.pato_cloud_repo.install_file
+  ( p_file_path => 'admin/pm-no-sqlplus.sql'
+  , p_stop_on_error => true
+  , p_schema => 'ADMIN'
+  );
+  admin.pato_install_pkg.process_project
   ( p_path => 'sql'
   );
-  pato_install_pkg.process_project
+  admin.pato_install_pkg.process_project
   ( p_path => 'apex'
   );
 end;
